8303738d618c57e609a3ac08edcece58c7f60bc4
Anthony Magrene <ekfhalomaster@gmail.com>
2021-03-28 11:25:10 -0400

    Update RSA.ps1

--- RSA.ps1
+++ RSA.ps1
@@ -58,13 +58,21 @@ while($stop -eq $false)
     foreach($R in $eRules){
         
         while($i -lt [math]::Sqrt($R)){
-        if(($R % $i) -eq 0){ 
-            write-host $R % $i
-            $cand += $i
-            $cand += $R/$i
+            if(($R % $i) -eq 0){ 
+                write-host $R % $i
+            
+                $cand += $i
+                $cand += $R/$i
+            }
+            $i++
         }
-    $i++
-    }
+        #$boolen=$i.GetType().Name
+        #write-host ([math]::Sqrt($R)).getType().name
+        
+        if( ([math]::Sqrt($R) %1 ) -eq 0){
+            $cand += $R
+        }
+        $i=1
     
     
     
